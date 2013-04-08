ad9ce649b357f4d97b0a57e742d59804300312e2
fukayatsu <fukayatsu@gmail.com>
2013-04-08 22:54:54 +0900

    add sublime text 2

--- modules/people/manifests/fukayatsu.pp
+++ modules/people/manifests/fukayatsu.pp
@@ -3,6 +3,7 @@ class people::fukayatsu {
   include skype
   include iterm2::stable #::devもある
   include chrome
+  include sublime_text_2
 
   # homebrewでインストール
   package {
@@ -11,6 +12,23 @@ class people::fukayatsu {
       'reattach-to-user-namespace',
       'tig'
     ]:
+  }
+
+  $home     = "/Users/${::luser}"
+  $src      = "${home}/src"
+  $dotfiles = "${src}/dotfiles"
+
+  # ~/src/dotfilesにGitHub上のfukayatsu/dotfilesリポジトリを
+  # git-cloneする。そのとき~/srcディレクトリがなければいけない。
+  repository { $dotfiles:
+    source  => "fukayatsu/dotfiles",
+    require => File[$src]
+  }
+  # git-cloneしたらインストールする
+  exec { "sh ${dotfiles}/symlink.sh":
+    cwd => $dotfiles,
+    creates => "${home}/.zshrc",
+    require => Repository[$dotfiles],
   }
 }
 
